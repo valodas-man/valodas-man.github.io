d59273b542ae185b183979c46b5003e19fd9143c
valodas-man <memebigboy008@gmail.com>
2026-09-19 22:52:57 +0300

    Add files via upload

---
+++ lists/15082026.js
@@ -0,0 +1,38 @@
+window.vocabularyLists = window.vocabularyLists || [];
+
+window.vocabularyLists.push({
+  name: "15082026",
+  words: [
+    ["unterstellen", "piedēvēt"],
+    ["ärgern", "dusmoties, kaitināt"],
+    ["legen", "nolikt"],
+    ["verschwinden", "pazust"],
+    ["gelten", " būt spēkā, stāties spēkā"],
+    ["beitragen", "veicināt, dot ieguldījumu"],
+    ["abwenden", "novērsties"],
+    ["belegen", "pierādīt, apliecināt"],
+    ["verändern", " mainīt, izmainīt"],
+    ["herausholen", " izņemt, iegūt"],
+    ["vertreten", "pārstāvēt"],
+    ["ratlos sein", " būt apjukušam"],
+    ["absteigen", "krist, izkrist, nokāpt"],
+    ["rutschen", " slīdēt"],
+    ["bestehen", "pastāvēt, izturēt, nokārtot"],
+    ["tun", "darīt"],
+    ["vermögen", "bagātība"],
+    ["verschieben", " pārbīdīt"],
+    ["gelingen", "izdoties"],
+    ["pflegen", "aprūpēt, kopt"],
+    ["driften", "slīdēt, novirzīties"],
+    ["dadurch", "tādējādi"],
+    ["Zusammenhalt haben","būt vienotiem, būt saliedētiem"],
+    ["einziehen","ievākties, ievilkt"],
+    ["beitragen", "dot ieguldījumu"],
+    ["ausmachen", "sarunāt, vienoties"],
+    ["leisten", "sniegt, veikt, atļauties finansiāli"],
+    ["Verantwortung", "atbildība"],
+    ["fordern", "pieprasīt"],
+    ["Glück", "veiksme"],
+
+  ]
+});
